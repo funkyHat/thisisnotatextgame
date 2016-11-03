0e7c1b36b8fecabbcd3e5c3a9854614857e0e65c
Matt Wheeler <m@funkyhat.org>
2016-11-03 19:52:08 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='This Is Not A Game',
+      version='1.0',
+      author_email='m@funkyhat.org',
+      url='https://github.com/funkyHat/thisisnotatextgame',
+      packages=['thisisnotatextgame'],
+
+      install_requires=['adventurelib'],
+     )
